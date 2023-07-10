bc9b83632858bc95399de4eb423f682483459515
xenedium <ahmed@abderraziq.com>
2023-07-10 12:46:48 +0100

    Hashmaps & quizz2

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -13,7 +13,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the basket.
         // Note that you are not allowed to put any type of fruit that's already
         // present!
+
+        match basket.get(&fruit) {
+            Some(count) => {},
+            None => {
+                basket.insert(fruit, 7);
+            }
+        }
+
     }
 }
 
